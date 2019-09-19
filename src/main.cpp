39c1d0a8e1d5975dc313fe112eca9743554928b7
fenoloftaleina <byldominik+fenoloftaleina@gmail.com>
2019-09-19 17:49:49 +0200

    Do cubes.

--- src/main.cpp
+++ src/main.cpp
@@ -36,9 +36,9 @@ bgfx::ShaderHandle loadShader(const char* _name) {
 }
 
 struct PosColorVertex {
-  float m_x;
-  float m_y;
-  float m_z;
+  float x;
+  float y;
+  float z;
   float r;
   float g;
   float b;
@@ -57,184 +57,159 @@ struct PosColorVertex {
 
 bgfx::VertexLayout PosColorVertex::ms_layout;
 
-static const int cubes_count = 17;
-static const int faces_count = cubes_count * 6;
-static const int triangles_count = faces_count * 2;
-static const int vertices_count = triangles_count * 3;
-static const int lines_per_face = 12;
-static const int lines_count = faces_count * lines_per_face;
-static PosColorVertex* s_cubeVertices = new PosColorVertex[vertices_count];
+static const int cubes_count = 1;
+static const int faces_per_cube_count = 6;
+static const int vertices_per_face_count = 4;
+static const int vertices_per_cube_count = faces_per_cube_count * vertices_per_face_count;
 
-static uint16_t* s_cubeLineList = new uint16_t[lines_count];
+static const int vertices_count = vertices_per_cube_count * cubes_count;
+static PosColorVertex* vertices = new PosColorVertex[vertices_count];
 
-void initLineList()
-{
-  int lineAccumulator = 0;
-  for(int i = 0; i < lines_count; i += lines_per_face) {
-    s_cubeLineList[i + 0] = 0 + lineAccumulator;
-    s_cubeLineList[i + 1] = 1 + lineAccumulator;
-    s_cubeLineList[i + 2] = 0 + lineAccumulator;
-    s_cubeLineList[i + 3] = 3 + lineAccumulator;
-    s_cubeLineList[i + 4] = 1 + lineAccumulator;
-    s_cubeLineList[i + 5] = 3 + lineAccumulator;
-    s_cubeLineList[i + 6] = 1 + lineAccumulator;
-    s_cubeLineList[i + 7] = 2 + lineAccumulator;
-    s_cubeLineList[i + 8] = 1 + lineAccumulator;
-    s_cubeLineList[i + 9] = 3 + lineAccumulator;
-    s_cubeLineList[i + 10] = 2 + lineAccumulator;
-    s_cubeLineList[i + 11] = 3 + lineAccumulator;
+static const int indices_per_face_count = 6;
 
-    lineAccumulator += 4;
-  }
-}
-      // {
-      // 0, 1,
-      // 0, 3,
-      // 1, 3,
-      // 1, 2,
-      // 1, 3,
-      // 2, 3,
-      //
-      // 4, 5,
-      // 4, 7,
-      // 5, 7,
-      // 5, 6,
-      // 5, 7,
-      // 6, 7,
-      // };
+static const int indices_count = indices_per_face_count * faces_per_cube_count * cubes_count;
+static uint16_t* indices = new uint16_t[indices_count];
 
+static const bx::Vec3 constexpr pos_vertices[vertices_per_cube_count] = {
+  // CCW, indices: 0, 1, 3, 1, 2, 3, means: 'u' shapes starting on the right
 
-// static const uint16_t s_cubeLineStrip[] =
-//   {
-//    0,1,3,
-//    1,2,3
-//   };
+  // front
+  {  1.0f,  1.0f, -1.0f,},
+  {  1.0f, -1.0f, -1.0f,},
+  { -1.0f, -1.0f, -1.0f,},
+  { -1.0f,  1.0f, -1.0f,},
 
-struct v3
-{
-  float x, y, z;
+  // right
+  {  1.0f,  1.0f,  1.0f,},
+  {  1.0f, -1.0f,  1.0f,},
+  {  1.0f, -1.0f, -1.0f,},
+  {  1.0f,  1.0f, -1.0f,},
+
+  // back
+  { -1.0f,  1.0f,  1.0f,},
+  { -1.0f, -1.0f,  1.0f,},
+  {  1.0f, -1.0f,  1.0f,},
+  {  1.0f,  1.0f,  1.0f,},
+
+  // left
+  { -1.0f,  1.0f, -1.0f,},
+  { -1.0f, -1.0f, -1.0f,},
+  { -1.0f, -1.0f,  1.0f,},
+  { -1.0f,  1.0f,  1.0f,},
+
+  //top
+  {  1.0f,  1.0f,  1.0f,},
+  {  1.0f,  1.0f, -1.0f,},
+  { -1.0f,  1.0f, -1.0f,},
+  { -1.0f,  1.0f,  1.0f,},
+
+  //bottom
+  { -1.0f, -1.0f,  1.0f,},
+  { -1.0f, -1.0f, -1.0f,},
+  {  1.0f, -1.0f, -1.0f,},
+  {  1.0f, -1.0f,  1.0f,},
 };
 
-void set6(int i, float x, float y, float z, float r, float g, float b)
+// static const bx::Vec3 constexpr col_vertices[vertices_per_cube_count] = {
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+//   {0.5f, 0.5f, 0.5f},
+// };
+
+struct Cube
 {
-  s_cubeVertices[i].m_x = x;
-  s_cubeVertices[i].m_y = y;
-  s_cubeVertices[i].m_z = z;
-  s_cubeVertices[i].r = r;
-  s_cubeVertices[i].g = g;
-  s_cubeVertices[i].b = b;
-}
+  // CubePosition pos;
+  // CubeColor col;
 
-struct C
-{
-  v3 v_lfu = {-0.5f,  0.5f, 0.5f};
-  v3 v_lfd = {-0.5f,  -0.5f, 0.5f};
-  v3 v_lbu = {-0.5f,  0.5f, -0.5f};
-  v3 v_lbd = {-0.5f,  -0.5f, -0.5f};
-  v3 v_rfu = {0.5f,  0.5f, 0.5f};
-  v3 v_rfd = {0.5f,  -0.5f, 0.5f};
-  v3 v_rbu = {0.5f,  0.5f, -0.5f};
-  v3 v_rbd = {0.5f,  -0.5f, -0.5f};
+  bx::Vec3 pos {0.0f, 0.0f, 0.0f};
+  bx::Vec3 col {0.7f, 0.7f, 0.7f};
 
-  v3 c_lfu = {-0.5f,  0.5f, 0.5f};
-  v3 c_lfd = {0.6f, 0.3f, 0.8f};
-  v3 c_lbu = {-0.5f,  0.5f, -0.5f};
-  v3 c_lbd = {-0.5f,  -0.5f, -0.5f};
-  v3 c_rfu = {0.5f, 0.5f, 0.5f};
-  v3 c_rfd = {0.2f, 0.7f, 0.5f};
-  v3 c_rbu = {0.5f,  0.5f, -0.5f};
-  v3 c_rbd = {0.5f,  -0.5f, -0.5f};
+  // vertices[0] = {  1.0f,  1.0f, 1.0f, 0.5f, 0.5f, 0.5f };
+  // vertices[1] = {  1.0f, -1.0f, 1.0f, 0.2f, 0.7f, 0.5f };
+  // vertices[2] = { -1.0f, -1.0f, 1.0f, 0.6f, 0.3f, 0.8f };
+  // vertices[3] = { -1.0f,  1.0f, 1.0f, 0.3f, 0.5f, 0.9f };
 
-  v3 p = {0.0f, 0.0f, 0.0f};
-  v3 s = {1.0f, 1.0f, 1.0f};
-
-  void set2v3(int i, v3 v, v3 c)
+  void writeVertex(int offset, int i)
   {
-    s_cubeVertices[i].m_x = (v.x + p.x) * s.x;
-    s_cubeVertices[i].m_y = (v.y + p.y) * s.y;
-    s_cubeVertices[i].m_z = (v.z + p.z) * s.z;
-    s_cubeVertices[i].r = c.x;
-    s_cubeVertices[i].g = c.y;
-    s_cubeVertices[i].b = c.z;
+    bx::Vec3 end_pos = bx::add(pos_vertices[i], pos);
+
+    printf("%d %d - %f %f %f\n", offset, i, end_pos.x, end_pos.y, end_pos.z);
+
+    vertices[offset + i].x = end_pos.x;
+    vertices[offset + i].y = end_pos.y;
+    vertices[offset + i].z = end_pos.z;
+    vertices[offset + i].r = col.x;
+    vertices[offset + i].g = col.y;
+    vertices[offset + i].b = col.z;
   }
 
-  void WriteBuffer(int offset)
+  void writeBuffer(int offset)
   {
-    set2v3(offset + 0, v_lfu, c_lfu);
-    set2v3(offset + 1, v_lfd, c_lfd);
-    set2v3(offset + 2, v_lbd, c_lbd);
-    set2v3(offset + 3, v_lbu, c_lbu);
-
-    set2v3(offset + 4, v_rfu, c_rfu);
-    set2v3(offset + 5, v_rfd, c_rfd);
-    set2v3(offset + 6, v_lfd, c_lfd);
-    set2v3(offset + 7, v_lfu, c_lfu);
-
-    set2v3(offset + 8, v_rfu, c_rfu);
-    set2v3(offset + 9, v_rbu, c_rbu);
-    set2v3(offset + 10, v_rbd, c_rbd);
-    set2v3(offset + 11, v_rfd, c_rfd);
-
-    set2v3(offset + 12, v_lbu, c_lbu);
-    set2v3(offset + 13, v_rbu, c_rbu);
-    set2v3(offset + 14, v_rbd, c_rbd);
-    set2v3(offset + 15, v_lbd, c_lbd);
-
-    set2v3(offset + 16, v_lbu, c_rfu);
-    set2v3(offset + 17, v_rbu, c_rbu);
-    set2v3(offset + 18, v_rfu, c_rbd);
-    set2v3(offset + 19, v_lfu, c_rfd);
-
-    set2v3(offset + 20, v_lbd, c_lbd);
-    set2v3(offset + 21, v_rbd, c_rbd);
-    set2v3(offset + 22, v_rfd, c_rfd);
-    set2v3(offset + 23, v_lfd, c_lfd);
-
-    // set6(0, 0.5f,  0.5f, 0.0f, 0.5f, 0.5f, 0.5f);
-    // set6(1, 0.5f, -0.5f, 0.0f, 0.2f, 0.7f, 0.5f);
-    // set6(2, -0.5f, -0.5f, 0.0f, 0.6f, 0.3f, 0.8f);
-    // set6(3, -0.5f,  0.5f, 0.0f, 0.3f, 0.5f, 0.9f);
+    for (int i = 0; i < vertices_per_cube_count; ++i) {
+      writeVertex(offset, i);
+    }
   }
 };
 
-C* cs = new C[cubes_count];
 
-void InitCubes()
+void initVertices()
 {
-  float scale = 0.3f;
-  for(int i = 0; i < cubes_count; ++i) {
-    cs[i].p.x = i / 1.0f;
-    cs[i].s.x = cs[i].s.y = cs[i].s.z = scale;
-    if (i == 6) {
-      cs[i].c_lfu = cs[i].c_lbu = cs[i].c_lbd = cs[i].c_lfd = {1.0f, 1.0f, 1.0f};
-    }
-    if (i == 5) {
-      cs[i].c_lfu = cs[i].c_lbu = cs[i].c_lbd = cs[i].c_lfd =
-        cs[i].c_rfu = cs[i].c_rbu = cs[i].c_rbd = cs[i].c_rfd =
-        {1.0f, 0.0f, 0.0f};
-    }
-    if (i == 4) {
-      cs[i].c_rfu = cs[i].c_rbu = cs[i].c_rbd = cs[i].c_rfd = {1.0f, 1.0f, 1.0f};
-    }
-    cs[i].WriteBuffer(i * 24);
-  }
+  // vertices[0] = {  1.0f,  1.0f, 1.0f, 0.5f, 0.5f, 0.5f };
+  // vertices[1] = {  1.0f, -1.0f, 1.0f, 0.2f, 0.7f, 0.5f };
+  // vertices[2] = { -1.0f, -1.0f, 1.0f, 0.6f, 0.3f, 0.8f };
+  // vertices[3] = { -1.0f,  1.0f, 1.0f, 0.3f, 0.5f, 0.9f };
 
-  initLineList();
+  Cube c;
+
+  c.writeBuffer(0);
 }
 
-  //  {  1.5f,  0.5f, 0.0f, 0.5f, 0.5f, 0.5f },
-  //  {  1.5f, -0.5f, 0.0f, 0.2f, 0.7f, 0.5f },
-  //  { 0.5f, -0.5f, 0.0f, 0.6f, 0.3f, 0.8f },
-  //  { 0.5f,  0.5f, 0.0f, 0.3f, 0.5f, 0.9f },
-  // };
+void initIndices()
+{
+  for (int i = 0; i < indices_count / 6; ++i) {
+    printf("indices %d\n", i);
+    indices[i * 6 + 0] = i * 4 + 0;
+    indices[i * 6 + 1] = i * 4 + 1;
+    indices[i * 6 + 2] = i * 4 + 3;
+    indices[i * 6 + 3] = i * 4 + 1;
+    indices[i * 6 + 4] = i * 4 + 2;
+    indices[i * 6 + 5] = i * 4 + 3;
+  }
+}
 
-// static const uint16_t s_cubeTriList[] =
-//   {
-//    0,1,3,
-//    1,2,3
-//   };
+void initShit()
+{
+  initVertices();
+  initIndices();
+}
 
-bgfx::DynamicVertexBufferHandle m_vbh;
+bgfx::VertexBufferHandle m_vbh;
 bgfx::IndexBufferHandle m_ibh;
 bgfx::ProgramHandle m_program;
 
@@ -296,17 +271,18 @@ int main (int argc, char* args[]) {
   // bgfx::init(bgfx::RendererType::Metal);
 
   PosColorVertex::init();
-  InitCubes();
-  m_vbh = bgfx::createDynamicVertexBuffer(
+  initShit();
+
+  m_vbh = bgfx::createVertexBuffer(
               // Static data can be passed with bgfx::makeRef
-              bgfx::makeRef(s_cubeVertices, vertices_count * sizeof(PosColorVertex)),
+              bgfx::makeRef(vertices, vertices_count * sizeof(vertices[0])),
               PosColorVertex::ms_layout
           );
 
   m_ibh = bgfx::createIndexBuffer(
               // Static data can be passed with bgfx::makeRef
-              // bgfx::makeRef(s_cubeTriList, sizeof(s_cubeTriList))
-              bgfx::makeRef(s_cubeLineList, lines_count * sizeof(uint16_t))
+              bgfx::makeRef(indices, indices_count * sizeof(indices[0]))
+              // bgfx::makeRef(indices, 6 * sizeof(indices[0]))
           );
 
   bgfx::ShaderHandle vsh = loadShader("bin/v_simple.bin");
@@ -350,8 +326,9 @@ int main (int argc, char* args[]) {
     dt = current_time - last_time;
 
     const bx::Vec3 at  = { 0.0f, 0.0f,   0.0f };
-    // const bx::Vec3 eye = { 0.0f, 0.0f, 7.0f };
-    const bx::Vec3 eye = { -5.0f, 4.0f, 7.0f };
+    // const bx::Vec3 eye = { -5.0f, 2.0f, -10.0f };
+    // const bx::Vec3 eye = { 5.0f, 2.0f, -10.0f };
+    const bx::Vec3 eye = {  10.0f, -10.0f, -13.0f };
 
     // Set view and projection matrix for view 0.
     float view[16];
@@ -402,9 +379,9 @@ int main (int argc, char* args[]) {
         | BGFX_STATE_WRITE_A
         | BGFX_STATE_WRITE_Z
         | BGFX_STATE_DEPTH_TEST_LESS
-        | BGFX_STATE_CULL_CW
+        | BGFX_STATE_CULL_CCW
         | BGFX_STATE_MSAA
-        | BGFX_STATE_PT_LINES
+        // | BGFX_STATE_PT_LINES
         // | BGFX_STATE_PT_LINESTRIP
         );
 
