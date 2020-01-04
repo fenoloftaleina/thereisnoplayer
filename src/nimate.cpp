4160266e6424b4f713dea5e820d1ce0e42b58a49
fenoloftaleina <byldominik+fenoloftaleina@gmail.com>
2020-01-04 13:24:45 +0100

    wip - animate vertices.

--- src/nimate.cpp
+++ src/nimate.cpp
@@ -61,6 +61,67 @@ void Nimate::run(
 }
 
 
+void Nimate::schedule_vertices(
+    const PosColorVertex* goal,
+    const int goal_count,
+    std::vector<std::vector<float>>& next_lengths,
+    std::vector<float>& lengths,
+    std::vector<float>& times
+    )
+{
+  if (next_vertices.empty()) {
+    times.resize(goal_size);
+    fr(i, times) {
+      times[i] = 0.0f;
+    }
+  }
+
+  next_vertices.push_back(goal);
+  next_vertices_count.push_back(goal_count);
+
+  next_lengths.push_back(lengths);
+}
+
+
+void Nimate::run_vertices(
+    const float dt,
+    PosColorVertex* output_value,
+    std::vector<float>& times,
+    std::vector<std::vector<float>>& next_lengths
+    )
+{
+  if (next_values.empty()) {
+    return;
+  }
+
+  finished_count = 0;
+
+  fr(i, output_value) {
+    times[i] += dt;
+    fraction = (times[i] / next_lengths[0][i]);
+    if (fraction > 1.0f) {
+      // next_fraction = fraction - 1.0f;
+      fraction = 1.0f;
+      finished_count += 1;
+    }
+    output_value[i] =
+      bx::add(
+          bx::mul(output_value[i], 1.0f - fraction),
+          bx::mul(next_values[0][i], fraction)
+          );
+  }
+
+  if (finished_count == output_value.size()) {
+    next_values.erase(next_values.begin());
+    next_lengths.erase(next_lengths.begin());
+    times.resize(next_values[0].size());
+    fr(i, times) {
+      times[i] = 0.0f;
+    }
+  }
+}
+
+
 void Nimate::abort(
     std::vector<std::vector<bx::Vec3>>& next_values,
     std::vector<float>& times
--- src/nimate.hpp
+++ src/nimate.hpp
@@ -6,16 +6,24 @@
 #include <bx/math.h>
 #include <vector>
 
+struct BufferObject;
+struct PosColorVertex;
+
 struct Nimate
 {
   std::vector<std::vector<bx::Vec3>> next_moving_positions;
   std::vector<std::vector<bx::Vec3>> next_moving_colors;
+
+  std::vector<std::vector<PosColorVertex>> next_moving_vertices;
 
   std::vector<float> moving_positions_times;
   std::vector<std::vector<float>> next_moving_positions_lengths;
 
   std::vector<float> moving_colors_times;
   std::vector<std::vector<float>> next_moving_colors_lengths;
+
+  std::vector<float> moving_vertices_times;
+  std::vector<std::vector<float>> next_moving_vertices_lengths;
 
   void schedule(
       std::vector<std::vector<bx::Vec3>>& next_values,
@@ -24,6 +32,14 @@ struct Nimate
       std::vector<float>& lengths,
       std::vector<float>& times
       );
+  void schedule_vertices(
+      const PosColorVertex* goal,
+      const int goal_count,
+      std::vector<std::vector<float>>& next_lengths,
+      std::vector<float>& lengths,
+      std::vector<float>& times
+      );
+
   void run(
       const float dt,
       std::vector<bx::Vec3>& output_value,
@@ -31,6 +47,7 @@ struct Nimate
       std::vector<float>& times,
       std::vector<std::vector<float>>& next_lenghts
       );
+
   void abort(
       std::vector<std::vector<bx::Vec3>>& next_values,
       std::vector<float>& times
@@ -43,5 +60,6 @@ struct Nimate
 };
 
 #include "world.hpp"
+#include "buffer_object.hpp"
 
 #endif
