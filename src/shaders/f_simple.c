055ef6edc1a95735debaba9c8a8e7e48dd8acf9d
fenoloftaleina <byldominik+fenoloftaleina@gmail.com>
2020-03-10 13:37:40 +0100

    Revert "Check out dithering."
    This reverts commit d9f2bc17e52c0a000365500492797e3ef43370c3.

--- src/shaders/f_simple.c
+++ src/shaders/f_simple.c
@@ -4,12 +4,6 @@ $input v_color0, v_normal0, v_position0
 
 uniform vec4 twh;
 
-float random(vec2 st) {
-  return fract(sin(dot(st.xy,
-          vec2(12.9898,78.233)))*
-      43758.5453123);
-}
-
 void main()
 {
   vec3 light_position = vec3(10.0, 10.0, -15.0);
@@ -21,10 +15,5 @@ void main()
   float diff = max(dot(v_normal0, light_direction), 0.0);
   vec3 diffuse = diff * light_color;
 
-
-  vec2 st = gl_FragCoord.xy;
-  st = st / twh.yz;
-
-
-	gl_FragColor = vec4(v_color0.xyz * (ambient + diffuse) + random(st) * 0.005, 1.0);
+	gl_FragColor = vec4(v_color0.xyz * (ambient + diffuse), 1.0);
 }
